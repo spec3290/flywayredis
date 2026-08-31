7db6c7700974e85e0793094a4c7434515e53ba70
spec3290 <spec3290@gmail.com>
2026-08-31 22:33:27 +0900

    feat: 전체조회 repository 메서드 추가

--- src/main/java/com/example/flywayredis/repository/ProductRepository.java
+++ src/main/java/com/example/flywayredis/repository/ProductRepository.java
@@ -4,5 +4,8 @@ import com.example.flywayredis.entity.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByOrderByCreatedAtDesc();
 }
